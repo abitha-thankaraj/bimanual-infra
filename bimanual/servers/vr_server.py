32477306f849f5000d09a2cda6ae0f5af990b92c
abitha-thankaraj <abitha7thankaraj@gmail.com>
2023-04-21 23:51:06 -0400

    lp commit

--- bimanual/servers/vr_server.py
+++ bimanual/servers/vr_server.py
@@ -44,37 +44,26 @@ def start_server(queue):
         if message.startswith("start"):
             start_teleop = True
             socket.send_string("OK: Starting teleop at time:{}".format(time.time()))
-            init_pos = True
-            continue    
-        
-        elif init_pos:
-            prev_left_pos, prev_right_pos = parse_pose_message(message)
+            init_pos_flag = True
+            
+        elif init_pos_flag == True:
+            init_left_pos, init_right_pos = parse_pose_message(message)
             last_ts = time.time()
-            init_pos = False
+            init_pos_flag = False
             socket.send_string("OK: Received init pos")
-            continue
-        
-        elif prev_left_pos is not None and prev_right_pos is not None and start_teleop:
+            
+        elif start_teleop == True:
             
             # Parse the message
-            left_pose, right_pose = parse_pose_message(message)
-
+            _ , right_pose = parse_pose_message(message)
 
             if time.time() - last_ts >= CONTROL_TIME_PERIOD:
-            
-                rel_left_pose[0] += (left_pose[0] - prev_left_pos[0])*SCALE_FACTOR
+
+                rel_right_pose[0] = (right_pose[0] - init_right_pos[0]) * SCALE_FACTOR
                 
-                # magnitude = np.linalg.norm(rel_left_pose)
-                print(np.linalg.norm(rel_left_pose[:3]))
-                if (np.linalg.norm(rel_left_pose[:3]) > 1):
-                    print("Sending to robot")
-                    # TODO: FIXME; HACK. quaternion so, 
-                    queue.put(CartesianMoveMessage(target=rel_left_pose[:6], wait=False, relative=True)) #For now, only consider the position
-                    # Reset the relative pose
-                    rel_left_pose = [0. for  _ in range(7)]
-                    # Update the previous pose
-                    prev_left_pos = left_pose
-                    prev_right_pos = right_pose
+                # print(np.linalg.norm(rel_right_pose[:3]))
+                # print("Sending to robot")
+                queue.put(CartesianMoveMessage(target=rel_right_pose[:3] + [0., 0., 0.], wait=False, relative=True)) #For now, only consider the position
 
             socket.send_string("OK: {}".format(time.time()))
 
@@ -98,9 +87,10 @@ def move_robot(queue, last_sent_msg_ts, last_sent_msg, control_timeperiod):
     arm.move_gohome(wait=True)
     time.sleep(0.1)
     
-    status, pose = arm.get_position_aa()
+    status, home_pose = arm.get_position_aa()
+    target_pose = [0]*6
     assert status==0, "Failed to get robot position"
-    print("Robot position: {}".format(pose))
+    print("Robot position: {}".format(home_pose))
     
     # exit()
     arm.set_mode(1)
@@ -109,40 +99,45 @@ def move_robot(queue, last_sent_msg_ts, last_sent_msg, control_timeperiod):
     time.sleep(0.1)
 
     while True: # Use a lock instead
-        if time.time() - last_sent_msg_ts.value > control_timeperiod:
-            print(queue.empty())
+        if (time.time() - last_sent_msg_ts.value) > control_timeperiod:
+            # print(queue.empty())
 
             if not queue.empty():
                 print("Sending message to robot: {}".format(queue.qsize()))
                 move_msg = queue.get()
-                print(move_msg)
+                # print(move_msg)
                 if move_msg.is_terminal():
                     break
                 #TODO : Move to target check for tolerance in step_size. - No; move this to the gym environment.
                 # print(vars(self))
-                print(move_msg.target)
-                print(vars(move_msg))
+                # print(move_msg.target)
+                # print(vars(move_msg))
 
                 # y = bot.get_position()
                 for i in range(len(move_msg.target)):
-                    pose[i]+=move_msg.target[i]
-                print("Sending robot to: {}".format(pose))
+                    target_pose[i] = move_msg.target[i] + home_pose[i]
+                # print("Sending robot to: {}".format(pose))
                 # x = arm.set_servo_cartesian_aa(pose, wait=False, relative=False, mvacc=200, speed=100)
 
-                if pose[0]>406 or pose[0]<206 or pose[1]>200 or pose[1]<-200:
-                    print("Robot out of bounds")
-                    if pose[0]>406:
-                        pose[0]=406
-                    elif pose[0] < 206:
-                        pose[0]=206
-                    if pose[1]>200:
-                        pose[1]=200
-                    elif pose[1] < -200:
-                        pose[1]=-200
-                    
-                    continue
+                
 
-                x = arm.set_servo_cartesian_aa(move_msg.target, wait=False, relative=True, mvacc=200, speed=100)
+                current_pose = arm.get_position_aa()[1]
+                delta_pose = np.array(target_pose[:3]) - np.array( current_pose[:3])
+                des_pose = (np.clip( delta_pose, -1, 1) + np.array( current_pose[:3])).tolist() + current_pose[3:]
+
+                if des_pose[0] > 406:
+                    des_pose[0]=406
+                elif des_pose[0] < 206:
+                    des_pose[0]=206
+                if des_pose[1]>200:
+                    des_pose[1]=200
+                elif des_pose[1] < -200:
+                    des_pose[1]=-200
+
+                print(des_pose)
+                x = arm.set_servo_cartesian_aa( des_pose, wait=False, relative=False, mvacc=100, speed=100)
+
+                # x = arm.set_servo_cartesian_aa(move_msg.target, wait=False, relative=True, mvacc=50, speed=50)
                 print("Robot set_servo_cartesian returns: {}".format(x))
                 if x!=0:
                     print("Failed to set robot position")
@@ -152,7 +147,7 @@ def move_robot(queue, last_sent_msg_ts, last_sent_msg, control_timeperiod):
 
                 last_sent_msg_ts = c_double(time.time())
                 last_sent_msg = move_msg
-                print("last_sent_msg: {}".format(last_sent_msg.target))
+                # print("last_sent_msg: {}".format(last_sent_msg.target))
 
 
             else:
@@ -164,6 +159,7 @@ if __name__ == "__main__":
 
     control_frequency = 90# TODO; measure control frequency for each box. COmmands should be sent at 30-250. If <30 - choppy; if>250 drop frames. Deatils in manual:
     control_timeperiod = 1./control_frequency
+
 
     register_ctx()
     ctx_manager = CustomManager(ctx=mp.get_context())
