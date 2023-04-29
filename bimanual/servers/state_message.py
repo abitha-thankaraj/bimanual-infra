f07e2e3722525daf217ead5e7e1fcc948629a429
abitha-thankaraj <abitha7thankaraj@gmail.com>
2023-04-29 12:00:39 -0400

    Align matrices

--- bimanual/servers/state_message.py
+++ bimanual/servers/state_message.py
@@ -12,9 +12,9 @@ class ControllerState:
     left_thumbstick: bool
     left_index_trigger: float
     left_hand_trigger: float
-    left_thumbstick_axes: np.ndarray  # Tuple[float, float]
-    left_local_position: np.ndarray  # Tuple[float, float, float]
-    left_local_rotation: np.ndarray  # Tuple[float, float, float, float]
+    left_thumbstick_axes: np.ndarray[Tuple[float, float]] 
+    left_local_position: np.ndarray[Tuple[float, float, float]]
+    left_local_rotation: np.ndarray[Tuple[float, float, float, float]]
 
     right_a: bool
     right_b: bool
@@ -22,25 +22,25 @@ class ControllerState:
     right_thumbstick: bool
     right_index_trigger: float
     right_hand_trigger: float
-    right_thumbstick_axes: np.ndarray  # Tuple[float, float]
-    right_local_position: np.ndarray  # Tuple[float, float, float]
-    right_local_rotation: np.ndarray  # Tuple[float, float, float, float]
+    right_thumbstick_axes: np.ndarray[Tuple[float, float]]
+    right_local_position: np.ndarray[Tuple[float, float, float]]
+    right_local_rotation: np.ndarray[Tuple[float, float, float, float]]
 
     @property
     def right_position(self) -> np.ndarray:
-        return self.get_aligned_position(self.right_local_position)
+        return self.right_affine[:3, 3]
 
     @property
     def left_position(self) -> np.ndarray:
-        return self.get_aligned_position(self.left_local_position)
+        return self.left_affine[:3, 3]
 
     @property
     def right_rotation_matrix(self) -> np.ndarray:
-        return self.get_aligned_rotation_matrix(self.right_local_rotation)
+        return self.right_affine[:3, :3]
 
     @property
     def left_rotation_matrix(self) -> np.ndarray:
-        return self.get_aligned_rotation_matrix(self.left_local_rotation)
+        return self.left_affine[:3, :3]
 
     @property
     def left_affine(self) -> np.ndarray:
@@ -50,21 +50,18 @@ class ControllerState:
     def right_affine(self) -> np.ndarray:
         return self.get_aligned_affine(self.right_local_position, self.right_local_rotation)
 
-    def get_aligned_rotation_matrix(self, controller_rotation, flip_matrix=FLIP_MATRIX):
-        """Returns the rotation matrix from the controller frame in VR (Quaternions) to rotation matrix.
-        x, y, z -> z, x, y
+    def get_aligned_affine(self, controller_position: np.ndarray, controller_rotation: np.ndarray):
+        """ To align the affine from the controller to the robot, coordinates
+        x, y, z -> z, x, y; Flip matrix is used to do that. We have no translations for now.
         """
-        return flip_matrix @ R.as_matrix(R.from_quat(controller_rotation))
 
-    def get_aligned_position(self, controller_position):
-        """ x, y, z -> z, x, y
-        """
-        # return einops.rearrange(controller_position, 'x y z -> z x y') 
-        return np.roll(controller_position, 1)
-
-    def get_aligned_affine(self, controller_position, controller_rotation):
-        return np.block([[self.get_aligned_rotation_matrix(controller_rotation), self.get_aligned_position(controller_position)[:, np.newaxis]],
-                         [np.zeros((1, 3)), 1]])
+        flip_affine = np.block([[FLIP_MATRIX, np.zeros((3, 1))],
+                                [np.zeros((1, 3)), 1]])
+        
+        controller_affine = np.block([[R.as_matrix(R.from_quat(controller_rotation)), controller_position[:, np.newaxis]],
+                                [np.zeros((1, 3)), 1.]])
+        
+        return flip_affine @ controller_affine
 
 
 def parse_controller_state(to_string_output: str) -> ControllerState:
@@ -84,15 +81,17 @@ def parse_controller_state(to_string_output: str) -> ControllerState:
 
     def parse_section(data: list) -> Tuple:
         return (
+            # Buttons
             parse_bool(data[0]),
             parse_bool(data[1]),
             parse_bool(data[2]),
             parse_bool(data[3]),
-
+            # Triggers
             parse_float(data[4]),
             parse_float(data[5]),
-
+            # Thumbstick
             parse_list_float(data[6]),
+            # Pose
             parse_list_float(data[7]),
             parse_list_float(data[8])
         )
@@ -105,6 +104,7 @@ def parse_controller_state(to_string_output: str) -> ControllerState:
 
 # TODO: Move this to a test file
 if __name__ == "__main__":
+    # Sample message
     test_msg = "Left Controller:;  Left X: True;  Left Y: False;  Left Menu: False;  Left Thumbstick: False;  Left Index Trigger: 0;  Left Hand Trigger: 0;  Left Thumbstick Axes: 0,0;  Left Local Position: -0.6630062,0.7440274,0.08777055;  Left Local Rotation: 0.1541033,-0.04510121,0.5017885,0.8499568;|Right Controller:;  Right A: False;  Right B: False;  Right Menu: False;  Right Thumbstick: False;  Right Index Trigger: 0;  Right Hand Trigger: 0;  Right Thumbstick Axes: 0,0;  Right Local Position: -0.5966942,0.749879,0.1490001;  Right Local Rotation: 0.1249516,0.1079503,0.3456937,0.9237044;"
     controller_state = parse_controller_state(test_msg)
     from IPython import embed
