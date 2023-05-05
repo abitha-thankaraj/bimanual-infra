7eae4dc390ac1ae00a37dcce8937580119c13786
abitha-thankaraj <abitha7thankaraj@gmail.com>
2023-05-05 19:03:37 -0400

    Added latency benchmarking

--- scripts/benchmark_robot_commands.py
+++ scripts/benchmark_robot_commands.py
@@ -1,16 +1,11 @@
-from typing import Callable
+import pandas as pd
 from bimanual.hardware.robot import Robot
-from bimanual.utils.debug_utils import DebugTimer
+from bimanual.utils.debug_utils import DebugTimer, benchmark_cmd
 
-
-def benchmark_cmd(n_trials: int, cmd: Callable[[], None], name: str) -> None:
-    intervals = []
-    for _ in range(n_trials):
-        with DebugTimer(name) as ds:
-            cmd()
-        intervals.append(ds.interval)
-    print("Avg time taken for {} : {}".format(
-        name, sum(intervals)/len(intervals)))
+def concat_states(robot: Robot, n_states=1000):
+    s = robot.get_current_state_action_tuple().to_df()
+    for _ in range(n_states):
+        s = pd.concat([s, robot.get_current_state_action_tuple().to_df()])
 
 
 if __name__ == "__main__":
@@ -21,27 +16,22 @@ if __name__ == "__main__":
     n_trials = 10
 
     benchmark_cmd(n_trials, robot.get_current_state_action_tuple,
-                  "Robot get s,a tuples")
-    benchmark_cmd(n_trials, robot.get_servo_angle, "Robot get js")
-    benchmark_cmd(n_trials, robot.get_position_aa, "Robot get pose")
-    benchmark_cmd(n_trials, robot.get_gripper_position, "Robot get gripper")
+                  name="Robot get s,a tuples")
+    benchmark_cmd(n_trials, robot.get_servo_angle, name="Robot get js")
+    benchmark_cmd(n_trials, robot.get_position_aa, name="Robot get pose")
+    benchmark_cmd(n_trials, robot.get_gripper_position,
+                  name="Robot get gripper")
+    # benchmark_cmd(n_trials, concat_states, kwargs={
+    #               "robot": robot}, name="conct states")
 
-    # for _ in range(n_trials):
-    #     with DebugTimer("Robot get s,a tuples") as ds:
-    #         robot.get_current_state_action_tuple()
-    #     print(ds.interval)
+    df_0 = robot.get_current_state_action_tuple().to_df()
+    df = robot.get_current_state_action_tuple().to_df()
+    for _ in range(100):
+        df = pd.concat([df, df_0])
 
-    # for _ in range(n_trials):
-    #     with DebugTimer("Robot get js"):
-    #         robot.get_servo_angle()[1]
-
-    # for _ in range(n_trials):
-    #     with DebugTimer("Robot get pose"):
-    #         robot.get_position_aa()[1]
-
-    # for _ in range(n_trials):
-    #     with DebugTimer("Robot get gripper"):
-    #         robot.get_gripper_position()[1]
-
+    with DebugTimer("df to dict") as dt:
+        for _ in range(1):
+            df = pd.concat([df, df_0])
+    print(dt.interval)
     from IPython import embed
     embed()
